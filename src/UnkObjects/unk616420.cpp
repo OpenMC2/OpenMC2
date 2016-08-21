bf00d681f6e050137bb530532b2f648377619236
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2016-08-21 09:11:02 +0100

    Added unk616420 the actual data reader

---
+++ src/UnkObjects/unk616420.cpp
@@ -0,0 +1,35 @@
+/**********************************************************************
+* OpenMC2 - An Open Source Re-Implementation of Midnight Club 2
+* Copyright (C) 2016  LRFLEW
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**********************************************************************/
+
+#include "unk616420.hpp"
+
+unk_616420::unk_616420(char *unk1, uint32_t unk2) {
+    this->unk9C = 0;
+    vtable = MC2_POINTER<void *>(0x0062FF40);
+    sub_615130(unk1, unk2);
+}
+    
+void unk_616420::sub_615130(char *unk1, uint32_t unk2) {
+    this->unk04 = unk1;
+    this->unk08 = 1;
+    this->unk0C = unk2;
+    this->unk10 = 32;
+    this->unk14 = 0;
+    this->unk18 = 0;
+    this->unk98 = 0;
+}
---
+++ src/UnkObjects/unk616420.hpp
@@ -0,0 +1,44 @@
+/**********************************************************************
+* OpenMC2 - An Open Source Re-Implementation of Midnight Club 2
+* Copyright (C) 2016  LRFLEW
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**********************************************************************/
+
+#pragma once
+
+#include "../Addresses.hpp"
+
+class unk_616420 {
+protected:
+    void *vtable;
+    char *unk04;
+    uint32_t unk08;
+    uint32_t unk0C;
+    uint32_t unk10;
+    uint32_t unk14;
+    uint8_t unk18;
+    uint8_t unk19[0x7F];
+    uint32_t unk98;
+    uint32_t unk9C;
+
+public:
+    unk_616420(char *unk1, uint32_t unk2);
+
+    void sub_615130(char *unk1, uint32_t unk2);
+
+    bool sub_6154D0(char *unk1, uint32_t unk2) {
+        return MC2_PROC_MEMBER<bool, unk_616420, char *, uint32_t>(0x006154D0, this, unk1, unk2);
+    }
+};
